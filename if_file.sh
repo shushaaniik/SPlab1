4a8c7db30e778ac6a01d99795c1bee530869f9df
shushaaniik <shushaniktovm@gmail.com>
2022-09-26 18:19:45 +0400

    Changed if_file.sh

--- if_file.sh
+++ if_file.sh
@@ -3,13 +3,20 @@
 echo "file path: "
 read file
 
-if [ -d $file ]
+len=$(echo $file | wc -m)
+
+if [ $len -eq 1 ] 
+then 
+ echo "no argument is passed"
+ exit 1
+elif [ -d $file ]
 then
- echo directory
+ echo "directory"
 elif [ -f $file ]
 then
  echo "regular file"
 else
  echo "a wrong path"
+ exit 1
 fi
 
