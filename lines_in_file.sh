feb8fc733652cdf9e585d4a6a1294eb6b4f2bad8
shushaaniik <shushaniktovm@gmail.com>
2022-09-26 15:39:55 +0400

    Made some changes 3

--- lines_in_file.sh
+++ lines_in_file.sh
@@ -9,11 +9,7 @@ if [ $# -eq 0 ]
 then
  echo "no argument is passed"
  exit 1
-elif ! [ -f $1 ]
-then 
- echo "file doesn't exist"
- exit 1
-elif ! [ -e $1 ]
+elif ! [ -f $1 ] | [ -e $1 ] 
 then
  echo "file doesn't exist"
  exit 1
